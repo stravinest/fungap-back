f493816af1c1ecfd286cf7088c75ebefcb50dd5f
criminal415 <denny415@naver.com>
2021-11-03 01:49:32 +0900

    코드 수정

--- __test__/admin-controller.spec.js
+++ __test__/admin-controller.spec.js
@@ -9,7 +9,7 @@ const {
   getUser,
 } = require('../controllers/admin');
 
-test('정상적인 접근이면 게시물리스트를 200응답과 함께 response', async () => {
+test('정상적인 접근이면 게시물리스트를 200응답과 함께 response', authenticateJWT, checkAuthority, async () => {
     await getBoard(req, res);
 
     expect(res.status).toBeCalledWith(200);
