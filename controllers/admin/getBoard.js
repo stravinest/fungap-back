25aa15921e93d33ad098410c98716f5d822df0e8
criminal415 <denny415@naver.com>
2021-11-04 22:43:44 +0900

    admin getBoard

--- controllers/admin/getBoard.js
+++ controllers/admin/getBoard.js
@@ -1,15 +1,21 @@
 const { User } = require('../../models');
 const { Board, Like } = require('../../models');
+const { sequelize } = require('../../models/board');
 
 const getBoardFunc = async (req, res) => {
   try {
     const user_id = req.userId;
     const userInfo = await User.findOne({ where: user_id });
-        
+
     const board = await Board.findAll({
-      include: [Like],
+      include: {
+        model: Like,
+        where: user_id,
+        attributes: [[sequelize.fn('COUNT', 'board_id'), 'like_count']],
+      },
+      raw: true,
     });
-    console.log(board.Like);
+    console.log(JSON.stringify(board));
     res.status(200).json({ result: 'success', board_list: board });
   } catch (err) {
     res.status(400).json({
