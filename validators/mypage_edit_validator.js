5512163f9b1ecd52459b964a53db3225f09c1354
shinsw627 <shinsw627@naver.com>
2021-11-09 19:14:24 +0900

    validator 수정

--- validators/mypage_edit_validator.js
+++ validators/mypage_edit_validator.js
@@ -1,6 +1,9 @@
 const Joi = require('joi');
 
 const userEditSchema = Joi.object({
+  user_image: Joi.string().empty('').messages({
+    'string.base': `"a" should be a type of 'text'`,
+  }),
   nickname: Joi.string().required().messages({
     'string.empty': `"a" cannot be an empty field`,
     'any.required': `"a" is a required field`,
