bb00ed2c7fb9c5b3327eef40232c2d8ea2181300
shinsw627 <shinsw627@naver.com>
2021-11-09 13:48:16 +0900

    유저수정 validator fix

--- validators/index.js
+++ validators/index.js
@@ -1,7 +1,9 @@
 const login = require('./login_validator');
 const signup = require('./signup_validator');
+const userEdit = require('./mypage_edit_validator');
 
 module.exports = {
   login,
   signup,
+  userEdit,
 };
