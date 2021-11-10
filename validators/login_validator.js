5b77b956dde8bf091146bbfe9c46e1bee602394d
LAPTOP-SMOR46KU\strav <stravinest@naver.com>
2021-11-11 00:01:15 +0900

    비밀번호 수정

--- validators/login_validator.js
+++ validators/login_validator.js
@@ -7,12 +7,17 @@ const loginSchema = Joi.object({
     'any.required': `필수 필드입니다...`,
     'string.base': `스트링이 아닙니다.`,
   }),
-  password: Joi.string().min(4).required().messages({
-    'string.base': `"a" should be a type of 'text'`,
-    'string.empty': `"a" cannot be an empty field`,
-    'string.min': `최소 네자리 수 이상이어야 합니다.`,
-    'any.required': `"a" is a required field`,
-  }),
+  password: Joi.string()
+    .regex(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/)
+    .min(8)
+    .required()
+    .messages({
+      'string.pattern.base': `비밀번호는 비밀번호는 최소 8 자이상, 최소 하나의 문자 및 하나의 숫자.`,
+      'string.min': `최소 8자 이상이어야 합니다.`,
+      'string.empty': `"a" cannot be an empty field`,
+      'string.min': `최소 네자리 수 이상이어야 합니다.`,
+      'any.required': `"a" is a required field`,
+    }),
 });
 
 module.exports = loginSchema;
