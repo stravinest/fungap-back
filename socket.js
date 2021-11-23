bbc71f1b13ff1439f23a7a2d39695530b3db38ef
shinsw627 <shinsw627@naver.com>
2021-11-23 23:16:27 +0900

    채팅 수정

--- socket.js
+++ socket.js
@@ -11,16 +11,17 @@ module.exports = (httpServer, app) => {
   app.set('io', io);
 
   //방의 유저수 알려주는 함수 (소켓 아이디 수 찾는 함수)
-  function countRoom(roomName) {
+  function countRoomUser(roomName) {
     return io.sockets.adapter.rooms.get(roomName)?.size;
   }
+
   let roomIUserList = [];
   let roomEUserList = [];
   let roomFUserList = [];
   let roomTUserList = [];
 
   //roomName에 맞는 리스트 리턴
-  function findRoomList(roomName) {
+  function findRoomUserList(roomName) {
     switch (roomName) {
       case 'I':
         return roomIUserList;
@@ -32,6 +33,45 @@ module.exports = (httpServer, app) => {
         return roomTUserList;
     }
   }
+
+  //유저가 나가면 유저리스트에서 빼는 함수
+  function spliceUserList(roomName, nickName, userId, index) {
+    switch (roomName) {
+      case 'I':
+        let roomIUseridList = [];
+        for (let i = 0; i < roomIUserList.length; i++) {
+          roomIUseridList.push(roomIUserList[i].userId);
+        }
+        index = roomIUseridList.indexOf(userId);
+        roomIUserList.splice(index, 1);
+        break;
+      case 'E':
+        let roomEUseridList = [];
+        for (let i = 0; i < roomEUserList.length; i++) {
+          roomEUseridList.push(roomEUserList[i].userId);
+        }
+        index = roomEUseridList.indexOf(userId);
+        roomEUserList.splice(index, 1);
+        break;
+      case 'F':
+        let roomFUseridList = [];
+        for (let i = 0; i < roomFUserList.length; i++) {
+          roomFUseridList.push(roomFUserList[i].userId);
+        }
+        index = roomFUseridList.indexOf(userId);
+        roomFUserList.splice(index, 1);
+        break;
+      case 'T':
+        let roomTUseridList = [];
+        for (let i = 0; i < roomTUserList.length; i++) {
+          roomTUseridList.push(roomTUserList[i].userId);
+        }
+        index = roomTUseridList.indexOf(userId);
+        roomTUserList.splice(index, 1);
+        break;
+    }
+  }
+
   //처음 소켓서버에 접속하는 이벤트, 소켓을 하나씩 받는다.
   io.on('connection', (socket) => {
     //소켓에서의 미들웨어 느낌  감시자
@@ -53,18 +93,22 @@ module.exports = (httpServer, app) => {
           if (!roomIUserList.find((arr) => arr.userId === userId)) {
             roomIUserList.push({ nickName: nickName, userId: userId });
           }
+          break;
         case 'E':
           if (!roomEUserList.find((arr) => arr.userId === userId)) {
             roomEUserList.push({ nickName: nickName, userId: userId });
           }
+          break;
         case 'F':
           if (!roomFUserList.find((arr) => arr.userId === userId)) {
             roomFUserList.push({ nickName: nickName, userId: userId });
           }
+          break;
         case 'T':
           if (!roomTUserList.find((arr) => arr.userId === userId)) {
             roomTUserList.push({ nickName: nickName, userId: userId });
           }
+          break;
       }
 
       //방에 접속함을 알리는 이벤트
@@ -73,8 +117,8 @@ module.exports = (httpServer, app) => {
         .emit(
           'notice_user_join',
           nickName,
-          findRoomList(roomName),
-          countRoom(roomName)
+          findRoomUserList(roomName),
+          countRoomUser(roomName)
         );
     });
 
@@ -83,45 +127,62 @@ module.exports = (httpServer, app) => {
       socket.rooms.forEach((room) =>
         socket
           .to(room)
-          .emit('notice_user_disconnect', room, countRoom(room) - 1)
+          .emit('notice_user_disconnect', room, countRoomUser(room) - 1)
       );
     });
 
     //유저가 룸 떠났을 때 발생하는 이벤트
     socket.on('user_left', (roomName, nickName, userId) => {
       let index;
+
       //roomName에 맞는 유저리스트에서 삭제
-      switch (roomName) {
-        case 'I':
-          index = roomIUserList.indexOf({
-            nickName: nickName,
-            userId: userId,
-          });
-          roomIUserList.splice(index, 1);
-        case 'E':
-          index = roomEUserList.indexOf({
-            nickName: nickName,
-            userId: userId,
-          });
-          roomEUserList.splice(index, 1);
-        case 'F':
-          index = roomFUserList.indexOf({
-            nickName: nickName,
-            userId: userId,
-          });
-          roomFUserList.splice(index, 1);
-        case 'T':
-          index = roomTUserList.indexOf({
-            nickName: nickName,
-            userId: userId,
-          });
-          roomTUserList.splice(index, 1);
-      }
+      spliceUserList(roomName, nickName, userId, index);
+
       io.sockets
         .to(roomName)
         .emit('notice_user_left', roomName, nickName, userId);
 
       socket.leave(roomName);
+    });
+
+    //유저 목록 보내는 이벤트
+    socket.on('current_usercount', async (roomName) => {
+      let userList = findRoomUserList(roomName);
+
+      let targetRoomNameSequelizeQuerys = [];
+
+      let resultPromiseall;
+      if (userList.length > 0) {
+        for (let i = 0; i < userList.length; i++) {
+          const targetUserId = userList[i].userId;
+          const targetNickName = userList[i].nickName;
+          const targetRoomNameSequelizeQuery = new Promise(
+            (resolve, reject) => {
+              resolve(
+                sequelize.query(
+                  `SELECT nickname, user_image FROM database_final_project.users where user_id = ${targetUserId} and nickname = '${targetNickName}'`,
+                  {
+                    type: Sequelize.QueryTypes.SELECT,
+                  }
+                )
+              );
+            }
+          );
+          targetRoomNameSequelizeQuerys.push(targetRoomNameSequelizeQuery);
+        }
+      }
+
+      await Promise.all(targetRoomNameSequelizeQuerys).then((values) => {
+        resultPromiseall = values;
+        console.log(values);
+      });
+
+      console.log(roomName);
+      socket.emit(
+        'current_usercount',
+        resultPromiseall,
+        countRoomUser(roomName)
+      );
     });
 
     //메세지 보내는 이벤트
