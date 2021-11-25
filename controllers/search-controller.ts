457171479b9567240e62033b2307314d2cd37b69
LAPTOP-SMOR46KU\strav <stravinest@naver.com>
2021-11-25 22:46:52 +0900

    배포

--- controllers/search-controller.ts
+++ controllers/search-controller.ts
@@ -5,7 +5,7 @@ import { errors } from '@elastic/elasticsearch';
 import { Request, Response, NextFunction } from 'express';
 
 const client = new Client({
-  node: process.env.elastic_node,
+  node: 'http://34.64.168.183:9200',
   auth: {
     username: process.env.elastic_username!,
     password: process.env.elastic_password!,
