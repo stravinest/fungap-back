db1428d321c76b7bfc7fba7556d5078166a62036
shinsw627 <shinsw627@naver.com>
2021-11-27 01:03:55 +0900

    불필요한 import 삭제

--- controllers/allContent-controller.ts
+++ controllers/allContent-controller.ts
@@ -1,6 +1,5 @@
 import { Board, Game } from '../models';
 import { Request, Response } from 'express';
-import { any } from 'joi';
 
 const allContent = async (req: Request, res: Response) => {
   try {
@@ -8,14 +7,14 @@ const allContent = async (req: Request, res: Response) => {
     const async_board_list = Board.findAll({
       attributes: ['board_id', 'board_title', 'board_image'],
       order: [['createdAt', 'DESC']],
-      where: {board_delete_code:0},
+      where: { board_delete_code: 0 },
       limit: 6,
     });
 
     const async_game_list = Game.findAll({
       attributes: ['game_id', 'game_title', 'game_quest1', 'game_quest2'],
       order: [['createdAt', 'DESC']],
-      where: {game_delete_code:0},
+      where: { game_delete_code: 0 },
       limit: 6,
     });
     const promises: any = [async_board_list, async_game_list];
@@ -25,7 +24,6 @@ const allContent = async (req: Request, res: Response) => {
         game_list: result[1],
       });
     });
-
 
     //동기적 (await))
     // const async_board_list =await Board.findAll({
@@ -39,12 +37,11 @@ const allContent = async (req: Request, res: Response) => {
     //   order: [['createdAt', 'DESC']],
     //   limit: 6,
     // });
-   
+
     //   res.status(200).json({
     //     board_list: async_board_list,
     //     game_list: async_game_list,
     //   });
-
   } catch (error) {
     res.status(400).json({
       result: 'fail',
